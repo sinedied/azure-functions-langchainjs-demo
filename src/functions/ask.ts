b86a0a1d768667fa3c8b20bcb3d1bd884e6fdcab
sinedied <noda@free.fr>
2024-03-06 10:39:04 +0100

    feat: pick up local or azure version automatically

--- src/functions/ask.ts
+++ src/functions/ask.ts
@@ -1,17 +1,58 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { Readable } from "node:stream";
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
+import "dotenv/config";
+import { askYoutube as askYoutubeLocal } from "../local.js";
+import { askYoutube as askYoutubeAzure } from "../azure.js";
 
-export async function ask(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+export async function ask(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+  const body = (await request.json()) as any;
 
-    return { body: `Hello, ${name}!` };
-};
+  // Use local or Azure implementation
+  const useAzure = process.env.USE_AZURE === "true";
+  const askYoutube = useAzure ? askYoutubeAzure : askYoutubeLocal;
+
+  const chunks = await askYoutube(body?.question);
+
+  // Create a new stream buffer
+  const buffer = new Readable();
+  // We must implement the _read method, but we don't need to do anything
+  buffer._read = () => {};
+
+  // Do not await the otherwise the streaming won't work
+  streamData(chunks, buffer);
+
+  return {
+    body: buffer,
+    headers: {
+      "Content-Type": "application/x-ndjson",
+    },
+  };
+}
+
+async function streamData(chunks: AsyncIterable<any>, buffer: Readable) {
+  for await (const chunk of chunks) {
+    // Send JSON chunks, separated by newlines
+    buffer.push(JSON.stringify(chunk) + "\n");
+  }
+
+  // Signal end of stream
+  buffer.push(null);
+}
 
 app.setup({ enableHttpStream: true });
 
-app.http('ask', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: ask
+app.http("ask", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: ask,
 });
